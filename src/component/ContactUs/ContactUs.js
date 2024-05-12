ae9ff1ae82f3b501a4daa6e3180450fea0c9e719
1020akshita <akshita.524.gupta@gmail.com>
2024-05-12 12:45:47 +0530

    new project

---
+++ src/component/ContactUs/ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Contact.css'
+
+const ContactUs = () => {
+    return (
+        <section>
+            <div className="contact">
+                <h1>Contact Us</h1>
+                <p>A shoe is an item of footwear intended to protect and comfort the human foot. Though the human foot can adapt to varied terrains and climate conditions, it is vulnerable, and shoes provide protection. Form was originally tied to function, but over time, shoes also became fashion items.</p>
+            </div>
+
+            
+        </section>
+    )
+}
+
+export default ContactUs
